85f1087baa68d2182a2166eefce243d603255f9a
Denis Zakablukov <zakablukov_77@inbox.ru>
2022-11-20 13:22:02 +0400

    20.11.2022 Status BAR

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-# import time
-
+import time
 import requests
 from pprint import pprint
+from tqdm import tqdm
 
 # vk_id = input("Введите ID: ")
 vk_id = 210700286
+
+with open('token.txt', 'r') as file_object:
+    vk_token = file_object.read().strip()
+vk_url = 'https://api.vk.com/method/photos.get'
+params = {'owner_id': vk_id, 'album_id': 'profile', 'extended': 1, 'access_token': vk_token, 'v': '5.131'}
+
 with open('ya_token.txt', 'r') as file:
     yandex_token = file.read().strip()
 yandex_url = "https://cloud-api.yandex.net/v1/disk/resources"
@@ -17,27 +23,26 @@ yandex_headers = {
 def upload_file(url, name):
     params = {'path': name, 'url': url}
     info = requests.post(f"{yandex_url}/upload", params=params, headers=yandex_headers)
-    print(info.status_code)
-    # pprint(info.json())
+    # print(info.status_code)
+    # if info.status_code == 202:
+    #     print(f"Photo {name} saved")
+    # else:
+    #     print(f"Ошибка: {info.status_code}")
 
 
 def link_photo():
-    with open('token.txt', 'r') as file_object:
-        vk_token = file_object.read().strip()
-    vk_url = 'https://api.vk.com/method/photos.get'
-    params = {'owner_id': vk_id, 'album_id': 'profile', 'extended': 1, 'access_token': vk_token, 'v': '5.131'}
-    res = requests.get(vk_url, params=params)
-    print(f"STATUS CODE: {res.status_code}")
-    # pprint(res.json())
-    for val in res.json().values():
-        for i in val['items']:
+    foto = requests.get(vk_url, params=params)
+    print(f"STATUS CODE: {foto.status_code}")
+    print("Получаем URL на объекты")
+    for val in foto.json().values():
+        for i in tqdm(val['items']):
+            time.sleep(2)
             file_name = str(i['likes']['count']) + ".jpg"
             photo_url = i['sizes'][-1]['url']
             upload_file(photo_url, f"{vk_id}/{file_name}")
 
 
-def create_directory():
-    name_dir = input("Введите имя каталога:")
+def create_directory(name_dir):
     rez = requests.put(f'{yandex_url}?path={name_dir}', headers=yandex_headers)
     # print(rez.status_code)
     if rez.status_code == 201:
@@ -47,5 +52,5 @@ def create_directory():
 
 
 if __name__ == '__main__':
-    # create_directory()
+    create_directory(vk_id)
     link_photo()
