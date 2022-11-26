b578f9af0d772a092e2c667d1e9dffe825c338f1
Denis Zakablukov <zakablukov_77@inbox.ru>
2022-11-26 10:06:18 +0400

    20.11.2022_Requirements_txt

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import time
 import requests
-# from pprint import pprint
+from pprint import pprint
 from tqdm import tqdm
 
-# vk_id = input("Введите ID: ")
-vk_id = 210700286
+vk_id = input("Введите ID: ")
+# vk_id = 210700286
 
 with open('token.txt', 'r') as file_object:
     vk_token = file_object.read().strip()
@@ -25,6 +25,14 @@ yandex_headers = {
     'Authorization': f'OAuth {yandex_token}'
 }
 
+link_list = []
+
+
+def find_folder():
+    report = requests.get(f"{yandex_url}?path={vk_id}", headers=yandex_headers)
+    return report.status_code
+    # pprint(report.content.decode("utf-8"))
+
 
 def upload_file(url, name):
     params_info = {
@@ -34,7 +42,6 @@ def upload_file(url, name):
     info = requests.post(f"{yandex_url}/upload",
                          params=params_info,
                          headers=yandex_headers)
-    print(info.status_code)
     # if info.status_code == 202:
     #     print(f"Photo {name} saved")
     # else:
@@ -43,14 +50,24 @@ def upload_file(url, name):
 
 def link_photo():
     foto = requests.get(vk_url, params=params)
-    print(f"STATUS CODE: {foto.status_code}")
-    print("Получаем URL на объекты")
+    # print(f"STATUS CODE: {foto.status_code}")
+    # print("Получаем URL на объекты")
+    # pprint(foto.json())
     for val in foto.json().values():
-        for i in tqdm(val['items']):
-            time.sleep(1)
+        for i in tqdm(val['items'], desc='Идет загрузка фото'):
+            time.sleep(0.5)
             file_name = str(i['likes']['count']) + ".jpg"
             photo_url = i['sizes'][-1]['url']
+            photo_sizes = i['sizes'][-1]['type']
+            # print("\n", file_name, photo_sizes)
+            # print(photo_url, "\n", "="*10)
             upload_file(photo_url, f"{vk_id}/{file_name}")
+            # ==================================================
+            link_list.append({
+                "file_name": file_name,
+                "size": photo_sizes
+            })
+    print("Загрузка успешно завершена")
 
 
 def create_directory(name_dir):
@@ -62,5 +79,8 @@ def create_directory(name_dir):
 
 
 if __name__ == '__main__':
-    create_directory(vk_id)
-    link_photo()
+    if find_folder() == 200:
+        link_photo()
+    elif find_folder() == 404:
+        create_directory(vk_id)
+        link_photo()
